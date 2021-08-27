31af3c2d398f2672e7f121c941fcff03108209d7
jrojas10 <jrojasbf@gmail.com>
2021-08-27 10:32:35 -0700

    initial commit for rendering lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import Overview from "./components/Overview";
+import uniqid from "uniqid";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: {
+        text: '',
+        id: uniqid()
+      },
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: '',
+        id: uniqid()
+      },
+    });
+  };
+
+
+  //deconstructing
+
+  render() {
+    const { task, tasks } = this.state;
+
+    return (
+      <div className="container-fluid">
+        <form onSubmit={this.onSubmitTask}>
+          <div className="col col-md-2 mb-3">
+            <label className="form-label" htmlFor="taskInput">Enter Task</label>
+            <input className="form-control" type="text" id="taskinput" onChange={this.handleChange} value={task.text} id="taskInput" />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Add Task
+          </button>
+        </form>
+        <Overview tasks={tasks} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
